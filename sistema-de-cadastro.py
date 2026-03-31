d55eb6765916215058da3b4f7af4c6b17a0b1c21
Aluno <aluno@alunos.ce.senac.br>
2026-03-31 20:01:41 -0300

    nova atualização

--- sistema-de-cadastro.py
+++ sistema-de-cadastro.py
@@ -4,6 +4,7 @@ dicionario = {}
 
 while True:
     #Interface do sistema
+    print("------Menu do Sistema------")
     opcao1 = print("1 - Adicionar Contatos")
     opcao2 = print("2 - Listar Contatos")
     opcao3 = print("3 - Buscar Contatos")
@@ -22,13 +23,30 @@ while True:
         dicionario["email"] = email
     
         contatos.append(dicionario)
-    
+
+        print("\n Contato Adicionado! \n")
+
     elif escolha == "2":
        for dicionario in contatos:
+           print("------Lista de contatos------")
            print(contatos)
+           print("\n")
+    
+    elif escolha == "3":
+        busca = int(input("Digite o ID do contato:"))
+        detalhes = contatos[busca]
 
-
+        print(f"Nome:{detalhes[nome]} ")
+        print(f"Telefone:{detalhes[telefone]} ")
+        print(f"Email:{detalhes[email]} ")
         
+
+    elif escolha == "4":
+        print("Fim do programa...")
+        break
+        
+    else:
+        print("Opção Inválida")    
         
 
 
